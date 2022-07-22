e7169ad4fc4f4e71033e4dadad80abe71fc5d939
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-07-22 11:44:15 -0300

    Update Arrays e Objetos

---
+++ curso-javascript/Objetos-Arrays/11_push_pop/scripts.js
@@ -0,0 +1,12 @@
+let nomes = ["Jonathas", "Maria", "José"];
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido);
+console.log(nomes);
+
+nomes.push('Carlos');
+nomes.push('Ingrid');
+nomes.push('Luiz')
+
+console.log(nomes);
