8fac592ea7db028c6edfa56dfd77fea4d96eafb1
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-07-28 14:29:11 -0300

    Update explicação Expressões Regulares

--- curso-javascript/Expressões-Regulares/script.js
+++ curso-javascript/Expressões-Regulares/script.js
@@ -121,6 +121,7 @@ console.log(umOuMaisNumeros.test("asdfghjklç"));
 console.log(umOuMaisNumeros.test("1a2b3c4d5f6g"));
 */
 
+/*Operdores question (?)
 let opcional = /Prova\s?\d?/;
 
 console.log(opcional.test("Prova"));
@@ -137,5 +138,91 @@ const padrão2 = /\d+\w?/;
 console.log(padrão2.test("123"));
 console.log(padrão2.test("123a"));
 console.log(padrão2.test("123 "));
+*/
+
+/*Ocorrência precisa
+const cep = /\d{5}-\d{3}/;
+
+console.log(cep.test("88117-500"));
+console.log(cep.test("asd"));
+console.log(cep.test("881-50"));
+console.log(cep.test("99999-999"));
+
+const telefone = /\(\d{2}\)\d{4,5}-\d{4}/;
+
+console.log(telefone.test("(88))9874-8745"));
+console.log(telefone.test("(88))36987-1236"));
+*/
+/*Método Exec
+const dígitos = /\d+/;
+
+console.log(dígitos.exec("Tem o número 100 aqui."));
+console.log(digitos.exec("Tem o número aqui."));
+*/
+
+/*Método match
+const frase = "O número 100 está aqui".match(/\d+/);
+const frase2 = "O número está aqui".match(/\d+/);
+console.log(frase);
+console.log(frase2);
+*/
+
+/*Choice pattern
+let frutas = /\d+ (bananas|maçãs|laranjas)/;
+
+console.log(frutas.test("10 bananas"));
+console.log(frutas.test("25 batatas"));
+console.log(frutas.test("8 laranjas"));
+
+const reg = /\w+: (Jonathas|Jefferson|Junior)/;
+
+console.log(reg.test("Nome: Jonathas"));
+console.log(reg.test("Nome: João"));
+console.log(reg.test("Nome: Junior"));
+*/
+/* Vaidando domínio
+const validarDomínio = /www.\w+\.com|com.br/; //Com WWW
+
+console.log(validarDomínio.test("www.google.com"));
+console.log(validarDomínio.test("www.test"));
+console.log(validarDomínio.test("testando.com"));
+console.log(validarDomínio.test("www.horadecodar.com.br"));
+console.log(validarDomínio.test("www.horadecodar"));
+
+const validaDomínio = /[?www.]\w+\.com|com.br/; //sem WWW
 
 
+console.log(validaDomínio.test("www.google.com"));
+console.log(validaDomínio.test("www.test"));
+console.log(validaDomínio.test("testando.com"));
+console.log(validaDomínio.test("www.horadecodar.com.br"));
+console.log(validaDomínio.test("www.horadecodar")); 
+*/
+
+/* Validar email
+let validarEmail = /\w+@\w+\.\w+/;
+
+console.log(validarEmail.test("test@email.com"))
+console.log(validarEmail.test("email@email"))
+console.log(validarEmail.test("ronaldo@yahoo.com"))
+console.log(validarEmail.test("email.com"))
+
+const validaEmail = /\w+@\w+\.\w+/;
+
+console.log(validaEmail.test("jonathasrosa85@gmail.com"));
+console.log(validaEmail.test("matheus@gmail"));
+console.log(validaEmail.test("gmail.com"));
+console.log(validaEmail.test("jonathasrosa85@gmail.com.br"));
+console.log(validaEmail.test("gmail@gmail.gmail.gmail"));
+*/
+
+/*Validar data de nascimento
+let validarDataDeNascimento = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}/;
+//let validarDataDeNascimento = /^[1-31]{2}[/][1-12]{2}[/][1920-2022]{4}/;
+
+console.log(validarDataDeNascimento.test('01/05/2000'));
+console.log(validarDataDeNascimento.test('10/6/2025'));
+console.log(validarDataDeNascimento.test('1/08/2018'));
+console.log(validarDataDeNascimento.test('31/13/1985'));
+console.log(validarDataDeNascimento.test('99/99/9999'));
+*/
