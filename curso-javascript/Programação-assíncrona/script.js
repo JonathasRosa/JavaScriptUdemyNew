df77ec929f4b8cbb2b2650d473447e84db650c27
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-08-01 11:57:29 -0300

    Update explicações sobre programação assíncrona

--- curso-javascript/Programação-assíncrona/script.js
+++ curso-javascript/Programação-assíncrona/script.js
@@ -96,4 +96,51 @@ console.log(somar(2, 4));
 
 somar(2, 4).then(value => console.log(value));*/
 
-function somaComDeley(a,b)
+/* EX-1: Await
+function somaComDelay(a, b) {
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve(a + b);
+        }, 4000);
+    })
+}
+async function resSoma(a, b, c) {
+    let x = somaComDelay(a, b);
+    let y = c;
+    return await x + y;
+}
+resSoma(1, 2, 3).then(value => console.log(value)); 
+EX-2: Await
+function somaComDelay(a,b) {
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve(a + b);
+        }, 2000);
+    });
+};
+
+async function soma(a, b, c, d) {
+    let x = somaComDelay(a, b);
+    let y = somaComDelay(c, d);
+    
+    return await x + await y;
+}
+soma(2, 4, 6, 8).then(y => console.log(y));
+*/
+
+function* criadorId() {
+    let id = 0;
+    while (true) {
+        yield id++;
+    }
+}
+let criaId = criadorId();
+
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
+console.log(criaId.next().value);
